0a90a307a21eda69d387d37f7a0486635d69309d
ravikuys <ravi75@gmail.com>
2020-07-05 08:49:53 +0530

    First commit

---
+++ sampleScalaPrograms/src/com/learning/scala/structuredTypes.scala
@@ -0,0 +1,8 @@
+package com.learning.scala
+
+object structuredTypes {
+  def main(args:Array[String]){
+    println("Structured Types sample in scala and spark") 
+    println("Creating spark context")
+  }
+}
